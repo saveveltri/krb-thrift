04ba8710d1aa1de6b308f38b2815f02b51b14e17
robbenti <roberto.bentivoglio@radicalbit.io>
2016-03-11 12:27:40 +0100

    Updated classes

--- src/main/java/joshelser/SecurityUtils.java
+++ src/main/java/joshelser/SecurityUtils.java
@@ -11,7 +11,7 @@ import java.security.PrivilegedExceptionAction;
 /**
  * A utility class that lets program code run in a security context provided by the
  * Hadoop security user groups.
- *
+ * <p/>
  * The secure context will for example pick up authentication information from Kerberos.
  */
 public final class SecurityUtils {
@@ -20,11 +20,11 @@ public final class SecurityUtils {
 
     // load Hadoop configuration when loading the security utils.
     public static Configuration hdConf = new Configuration();
-    {
-        hdConf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
-        hdConf.set("fs.file.impl", LocalFileSystem.class.getName());
-        hdConf.set("hadoop.security.authentication", "KERBEROS");
-    }
+//    {
+//        hdConf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+//        hdConf.set("fs.file.impl", LocalFileSystem.class.getName());
+//        hdConf.set("hadoop.security.authentication", "KERBEROS");
+//    }
 
     public static boolean isSecurityEnabled() {
         UserGroupInformation.setConfiguration(hdConf);
@@ -34,6 +34,7 @@ public final class SecurityUtils {
     public static <T> T runSecured(final FlinkSecuredRunner<T> runner) throws Exception {
         UserGroupInformation.setConfiguration(hdConf);
         UserGroupInformation ugi = UserGroupInformation.getCurrentUser();
+        System.out.println("########## ===> UserGroupInformation.getCurrentUser() " + UserGroupInformation.getCurrentUser());
         if (!ugi.hasKerberosCredentials()) {
             new RuntimeException("Security is enabled but no Kerberos credentials have been found. " +
                     "You may authenticate using the kinit command.");
